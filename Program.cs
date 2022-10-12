bbb3d5884c8e0b6f5e6c59d9d255f4c5438e05a5
Viktor Mehl <vikt2553@mail.hansenberg.dk>
2022-10-12 14:58:39 +0200

    made car color

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿Car car1 = new Car("BMW");
 
+Console.WriteLine("What is the color of your car?");
+car1.color = Console.ReadLine();
+
+Console.WriteLine("your car is" + car1.color);
+
+Console.ReadLine();
