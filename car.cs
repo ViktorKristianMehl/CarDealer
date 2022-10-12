6628ba49b460bffea5b2cdb936fe00210c45c968
Viktor Mehl <vikt2553@mail.hansenberg.dk>
2022-10-12 14:53:05 +0200

    Made car class

---
+++ car.cs
@@ -0,0 +1,18 @@
+
+class Car
+{
+
+    private string brand;
+    public string color;
+    public float Price = 0f;
+    public decimal damage = 0;
+    public float currentSpeed = 0f;
+    public float maxSpeed = 0f;
+
+    public Car(string test)
+    {
+        brand = test;
+        color = test;
+
+    }
+}
