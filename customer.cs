438ec1435c99ecf68eedc0a5ead26dc3d54711e3
Xip <casp578b@mail.hansenberg.dk>
2022-11-02 09:59:30 +0100

    Lavede noget customer

---
+++ customer.cs
@@ -0,0 +1,17 @@
+class Customer
+
+{
+    public decimal Condition = 0;
+    public float Money = 0f;
+    public float CarBrand = 0f;
+    public float Color = 0f;
+
+public Customer(float CarBrand) 
+{
+    CarBrand =  0f;
+}
+
+}
+
+ 
+
