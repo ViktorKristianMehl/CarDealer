45975b7d52416e0e2f7a97fe233780282337f2df
Viktor Mehl <vikt2553@mail.hansenberg.dk>
2022-12-08 10:59:03 +0100

    dealer class stuff

--- Dealer.cs
+++ Dealer.cs
@@ -1,6 +1,6 @@
 public class carDealer
 {
-
+    List<Car> randomCars = new List<Car>();
     float money = 0f;
 
     public carDealer(float money)
@@ -12,29 +12,45 @@ public class carDealer
     {
 
         Random RNG = new Random();
-        List<Car> randomCars = new List<Car>();
         int i = 0;
+        int e = 0;
         while (i < 10)
         {
 
             int brandNum = RNG.Next(0, 9);
             int colorNum = RNG.Next(0, 9);
+            float damage = RNG.Next(0, 100);
+            float price = RNG.Next(1000, 20000);
+            float maxspeed = RNG.Next(80, 300);
 
 
+            randomCars.Add(new Car(StaticCars.CarBrands[brandNum], StaticCars.CarColors[colorNum], damage, price, maxspeed));
 
 
-            randomCars.Add(new Car(StaticCars.CarBrands[brandNum], StaticCars.CarColors[colorNum]));
-
-
-            Console.WriteLine(StaticCars.CarBrands[brandNum] + " " + StaticCars.CarColors[colorNum]);
+            Console.WriteLine(
+            "------------------ \n" +
+            "Brand: " + StaticCars.CarBrands[brandNum] +
+            "\nColor: "
+            + StaticCars.CarColors[colorNum] +
+            "\nDamage: "
+            + damage
+            + "% \n"
+            + "Price: " + price + "$ \n");
 
             i++;
 
         }
 
     }
-    public void AdvancedStats()
+
+    public void showList()
     {
+        int i = 0;
+        while (i < 10)
+        {
+            Console.WriteLine(randomCars);
+            i++;
+        }
 
     }
 
--- car.cs
+++ car.cs
@@ -1,21 +1,17 @@
 
-class Car
+public class Car
 {
 
     public string brand;
     public string color;
     public float price = 0f;
-    public decimal damage = 0;
+    public float damage = 0;
     public float currentSpeed = 0f;
     public float maxSpeed = 0f;
     public bool Driving = false;
 
-    public Car(string test, string placeholder)
+    public Car(string test, string placeholder, float price, float damage, float maxSpeed)
     {
-        Random carRNG = new Random();
-        decimal damage = carRNG.Next(0, 100);
-        float price = carRNG.Next(1000, 20000);
-        float maxspeed = carRNG.Next(80, 300);
         brand = test;
         color = placeholder;
 
