cfabdc396164d05cc095923292293dd44cffbb3b
Xip Desktop <cabec2005@gmail.com>
2022-12-31 12:54:03 +0100

    Buy og sell Method
    Har lavet buy og sell method, implementeret buy method i progam.cs samt lavet et fix på listen af vores biler så vi ikke får en ny liste hver gang, men at vi får den samme liste hver gang som er generated.

--- customer.cs
+++ customer.cs
@@ -5,21 +5,41 @@ public class Customer
     public float Money = 200f;
     public float CarBrand = 0f;
     public float Color = 0f;
-
+    public int carCount = 0;
+    public Dictionary<int, Car> ownedCars = new Dictionary<int, Car>();
+   
     public Customer(float startmoney)
     {
         Money = startmoney;
     }
 
-    public void BuyMethod(float price, Car vc)
+    //vores buy method
+    public bool purchaseCar(int carNumber, Car car)
     {
+        if (!ownedCars.TryAdd(carNumber, car)) 
+            return false;
 
-        Money = price;
-        Money = Money - price;
+        Money = Money - car.Price;
+        carCount++; 
 
+        return true; 
+    }
+
+// vores sell method
+   public bool SellCar(int carNumber, Car car)
+    {
+        
+        if (!ownedCars.Remove(carNumber)) 
+            return false;
+
+        Money = Money + ownedCars[carNumber].Price;
+        carCount--; 
+
+        return true; 
     }
 
 }
+
 
 
 
